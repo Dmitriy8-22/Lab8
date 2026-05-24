0669d0458e80e5d7f1955d204e48651b195e7b0d
Dmitriy8-22 <dmitrijkiselev36@gmail.com>
2026-05-24 17:17:01 +0300

    Lab8

--- app/models.py
+++ app/models.py
@@ -1,19 +1,26 @@
 import os
 import mysql.connector
+from mysql.connector.constants import ClientFlag
 
 class ItemModel:
     def __init__(self):
         self.config = {
-            'host': os.getenv('DB_HOST', os.getenv('DB_HOST')),
-            'user': os.getenv('DB_USER', os.getenv('DB_USER')),
-            'password': os.getenv('DB_PASS', os.getenv('DB_PASS')),
-            'database': os.getenv('DB_NAME', os.getenv('DB_NAME'))
+            'host': os.getenv('DB_HOST', 'localhost'),
+            'user': os.getenv('DB_USER', 'root'),
+            'password': os.getenv('DB_PASS', 'rootpassword'),
+            'database': os.getenv('DB_NAME', 'mydb'),
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+            'collation': 'utf8mb4_unicode_ci'
         }
 
     def get_all_items(self):
         try:
             conn = mysql.connector.connect(**self.config)
-            cursor = conn.cursor(dictionary=True)
+            cursor = conn.cursor(dictionary=True, buffered=True)
+            cursor.execute("SET NAMES utf8mb4")
+            cursor.execute("SET CHARACTER SET utf8mb4")
+            cursor.execute("SET character_set_connection=utf8mb4")
             cursor.execute('SELECT name FROM items')
             items = cursor.fetchall()
             cursor.close()
@@ -22,4 +29,3 @@ class ItemModel:
         except Exception as e:
             print(f"Error: {e}")
             return []
-
